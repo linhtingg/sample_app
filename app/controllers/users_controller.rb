d681b0bc1f8f3bb91ae64971608592aef69b4676
Hoàng Linh <116622444+linhtingg@users.noreply.github.com>
2023-09-12 10:29:06 +0700

    Add mobile styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      reset_session
+      log_in @user
+      
       flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
       else
       render :new, status: :unprocessable_entity
     end
--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,10 +1,18 @@
 // Menu manipulation
 // Add toggle listeners to listen for clicks.
 document.addEventListener("turbo:load", function () {
-    let account = document.querySelector("#account");
-    account.addEventListener("click", function (event) {
-        event.preventDefault();
-        let menu = document.querySelector("#dropdown-menu");
-        menu.classList.toggle("active");
+
+    function addToggleListener(selected_id, menu_id, toggle_class) {
+        let selected_element = document.querySelector(`#${selected_id}`);
+        selected_element.addEventListener("click", function (event) {
+            event.preventDefault();
+            let menu = document.querySelector(`#${menu_id}`)
+            menu.classList.toggle(toggle_class);
+        });
+    }
+    // Add toggle listeners to listen for clicks of different types
+    document.addEventListener("turbo:load", function () {
+        addToggleListener("hamburger", "navbar-menu", "collapse");
+        addToggleListener("account", "dropdown-menu", "active");
     });
 });
