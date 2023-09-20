4f4e604f35518b33dde6c88a1f6ba5a46586d29d
Hoàng Linh <116622444+linhtingg@users.noreply.github.com>
2023-09-20 14:32:57 +0700

    Unifiy Indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,18 @@ class ApplicationController < ActionController::Base
     # to use all helper methods defined inside SessionsHelper
     include SessionsHelper
     include Pagy::Backend
-    
+
     def hello
         render html: "hello, world!"
     end
-    
+
     before_action :set_locale
     def set_locale
         I18n.locale = params[:locale] || I18n.default_locale
     end
-    
+
     def default_url_options
         {locale: I18n.locale}
     end
-       
+
 end
