019d2422237de612c771819272f336c857d74146
Hoàng Linh <116622444+linhtingg@users.noreply.github.com>
2023-09-22 09:24:10 +0700

    Create or Delete posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,29 @@
 class ApplicationController < ActionController::Base
-    # to use all helper methods defined inside SessionsHelper
-    include SessionsHelper
-    include Pagy::Backend
+	# to use all helper methods defined inside SessionsHelper
+	include SessionsHelper
+	include Pagy::Backend
 
-    def hello
-        render html: "hello, world!"
-    end
+	def hello
+		render html: "hello, world!"
+	end
 
-    before_action :set_locale
-    def set_locale
-        I18n.locale = params[:locale] || I18n.default_locale
-    end
+	before_action :set_locale
+	def set_locale
+		I18n.locale = params[:locale] || I18n.default_locale
+	end
 
-    def default_url_options
-        {locale: I18n.locale}
-    end
+	def default_url_options
+		{locale: I18n.locale}
+	end
+
+	private
+		# Confirms a logged-in user.
+	def logged_in_user
+		unless logged_in?
+			store_location
+			flash[:danger] = "Please log in."
+			redirect_to login_url, status: :see_other
+		end
+	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,21 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[edit update index destroy]
   before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: %i[destroy]
-  
+
   def index
 		# @users = User.paginate(page: params[:page])
-    @pagy, @users = pagy(User.all, items: 15) 
+    @pagy, @users = pagy(User.all, items: 15)
 	end
-  
+
   def new
     @user = User.new
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    # @microposts = @user.microposts.paginate(page: params[:page])
+    @pagy, @microposts = pagy(@user.microposts, items: 15)
+  end
 
   def create
     @user = User.new(user_params)
@@ -40,7 +44,7 @@ class UsersController < ApplicationController
       # Handle unsuccessful update
       render 'edit', status: :unprocessable_entity
     end
-  end   
+  end
 
   def destroy
     User.find(params[:id]).destroy
@@ -55,19 +59,9 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    # Before filters, confirms a logged-in user.
-    # Redirects to the login form if the user is not logged in.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
-
     # Confirms the correct user.
 		def correct_user
-			redirect_to(root_url, status: :see_other) unless @user == current_user      
+			redirect_to(root_url, status: :see_other) unless @user == current_user
 		end
 
     # Confirms an admin user.
